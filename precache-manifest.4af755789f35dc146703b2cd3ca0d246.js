460249c48ec2d457903d4fa69708b0617d61dd6b
Doyeon Baek <41532591+dosthcpp@users.noreply.github.com>
2024-03-09 16:27:18 +0900

    ":pencil: dbaek portfolio"

---
+++ precache-manifest.4af755789f35dc146703b2cd3ca0d246.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6894a38f5fe1546302da0bf094537ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6aae04a438f8f4244be",
+    "url": "/static/css/17.df879eac.chunk.css"
+  },
+  {
+    "revision": "5b2c9c214e8976d3c536",
+    "url": "/static/css/18.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "31efd805e9ab0db90cf6",
+    "url": "/static/css/main.fbd9f2eb.chunk.css"
+  },
+  {
+    "revision": "5b6822a79abcf12561be",
+    "url": "/static/js/16.c4cb9c7f.chunk.js"
+  },
+  {
+    "revision": "f6ae91cc732cb206d215457d28a56171",
+    "url": "/static/js/16.c4cb9c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6aae04a438f8f4244be",
+    "url": "/static/js/17.8e34ff1d.chunk.js"
+  },
+  {
+    "revision": "5b2c9c214e8976d3c536",
+    "url": "/static/js/18.e11e526c.chunk.js"
+  },
+  {
+    "revision": "31efd805e9ab0db90cf6",
+    "url": "/static/js/main.c7628d81.chunk.js"
+  },
+  {
+    "revision": "c49edd1005ee6c5f09de",
+    "url": "/static/js/reactPlayerDailyMotion.07c923da.chunk.js"
+  },
+  {
+    "revision": "322e582cdb8bdea4b374",
+    "url": "/static/js/reactPlayerFacebook.f83cec9f.chunk.js"
+  },
+  {
+    "revision": "e39ece9f641cdf3ee9ce",
+    "url": "/static/js/reactPlayerFilePlayer.ae1eb7fa.chunk.js"
+  },
+  {
+    "revision": "96f8d5e103d68022a745",
+    "url": "/static/js/reactPlayerKaltura.cc914816.chunk.js"
+  },
+  {
+    "revision": "144007683dad2b8fcbfa",
+    "url": "/static/js/reactPlayerMixcloud.0109dec2.chunk.js"
+  },
+  {
+    "revision": "aeba2e015bc687c05c5e",
+    "url": "/static/js/reactPlayerMux.80bd3506.chunk.js"
+  },
+  {
+    "revision": "698382fc37d674b074ab",
+    "url": "/static/js/reactPlayerPreview.408af712.chunk.js"
+  },
+  {
+    "revision": "76aad9017d7ba73305ca",
+    "url": "/static/js/reactPlayerSoundCloud.d47dcf4d.chunk.js"
+  },
+  {
+    "revision": "155dbb62553d01895790",
+    "url": "/static/js/reactPlayerStreamable.f77a5af7.chunk.js"
+  },
+  {
+    "revision": "4749e4596861269c0f36",
+    "url": "/static/js/reactPlayerTwitch.499c18cc.chunk.js"
+  },
+  {
+    "revision": "99aa7dbf6c723666242f",
+    "url": "/static/js/reactPlayerVidyard.28107624.chunk.js"
+  },
+  {
+    "revision": "05c49f4a4a4a3a832b32",
+    "url": "/static/js/reactPlayerVimeo.68a8761c.chunk.js"
+  },
+  {
+    "revision": "f33a99123935d9606866",
+    "url": "/static/js/reactPlayerWistia.a7a96857.chunk.js"
+  },
+  {
+    "revision": "f128572b62cadfc594f2",
+    "url": "/static/js/reactPlayerYouTube.825adaca.chunk.js"
+  },
+  {
+    "revision": "5101e06e4f0267a02012",
+    "url": "/static/js/runtime-main.b41f8e6e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "2b59a51a7b2eac8b6a9dba01235ac7d7",
+    "url": "/static/media/csharp.2b59a51a.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "c8329d89558391c2ab9e789d44612089",
+    "url": "/static/media/flutter.c8329d89.svg"
+  },
+  {
+    "revision": "2a87570040a9c8fc9e221347564f6b1c",
+    "url": "/static/media/iso.2a875700.png"
+  },
+  {
+    "revision": "2264cea169c325e353cd867133d0ecda",
+    "url": "/static/media/labLogo.2264cea1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "17c4d5e92461d3bc13db54ab83f270c7",
+    "url": "/static/media/marknova.17c4d5e9.png"
+  },
+  {
+    "revision": "d06b73283c703213e98c5d2aa0787ff3",
+    "url": "/static/media/pnu.d06b7328.png"
+  },
+  {
+    "revision": "8292c541822e89a264c89d3d281df132",
+    "url": "/static/media/sengchido.8292c541.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "17183b011292125dd11a177a698fd694",
+    "url": "/static/media/softwareLogo.17183b01.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b01562f94c2bcf68113e050fc0f8bdda",
+    "url": "/static/media/ventureLogo.b01562f9.jpeg"
+  }
+]);
